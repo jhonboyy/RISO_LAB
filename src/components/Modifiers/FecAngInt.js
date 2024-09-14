ae9ada90694b454aceee15530262d25a8a25a169
Jorge <hello@jhonboy.com>
2024-09-14 13:09:52 +0100

    🔨 REFACTOR - Scripts integration

--- src/components/Modifiers/FecAngInt.js
+++ src/components/Modifiers/FecAngInt.js
@@ -1,107 +1,118 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-// src/components/FrecuencyAngleIntensity.js
-export default function FrecuencyAngleIntensity({ actualizarVariables, checkEnter }) {
+export default function FrecuencyAngleIntensity() {
+  // Estado para los valores de frecuencia, ángulo e intensidad
   const [frecuencyRed, setFrecuencyRed] = useState(5);
   const [frecuencyGreen, setFrecuencyGreen] = useState(5);
   const [frecuencyBlue, setFrecuencyBlue] = useState(5);
+
   const [angleRed, setAngleRed] = useState(45);
   const [angleGreen, setAngleGreen] = useState(45);
   const [angleBlue, setAngleBlue] = useState(45);
+
   const [intensityRed, setIntensityRed] = useState(127);
   const [intensityGreen, setIntensityGreen] = useState(127);
   const [intensityBlue, setIntensityBlue] = useState(127);
 
   return (
     <div className='ValuesContainer'>
-      <div className="item grid">
-        <span id="frecuencyItem" style={{ cursor: 'help' }}>Frecuency</span>
+      <div className='item grid'>
+        <span id='frecuencyItem' style={{ cursor: 'help' }}>Frecuency</span>
       </div>
-      <div className="item grid">
-        <input
-          type="text"
-          value={frecuencyRed}
-          onChange={(e) => setFrecuencyRed(e.target.value)}
-          onBlur={actualizarVariables}
-          onKeyDown={(e) => checkEnter(e, 'frecuencyRed')}
+      <div className='item grid'>
+        <input 
+          type='text' 
+          id='frecuencyRed' 
+          value={frecuencyRed} 
+          onBlur={() => actualizarVariables()} 
+          onChange={(e) => setFrecuencyRed(e.target.value)} 
+          onKeyDown={(event) => checkEnter(event, 'frecuencyRed')} 
         />
       </div>
-      <div className="item grid">
-        <input
-          type="text"
-          value={frecuencyGreen}
-          onChange={(e) => setFrecuencyGreen(e.target.value)}
-          onBlur={actualizarVariables}
-          onKeyDown={(e) => checkEnter(e, 'frecuencyGreen')}
+      <div className='item grid'>
+        <input 
+          type='text' 
+          id='frecuencyGreen' 
+          value={frecuencyGreen} 
+          onBlur={() => actualizarVariables()} 
+          onChange={(e) => setFrecuencyGreen(e.target.value)} 
+          onKeyDown={(event) => checkEnter(event, 'frecuencyGreen')} 
         />
       </div>
-      <div className="item grid">
-        <input
-          type="text"
-          value={frecuencyBlue}
-          onChange={(e) => setFrecuencyBlue(e.target.value)}
-          onBlur={actualizarVariables}
-          onKeyDown={(e) => checkEnter(e, 'frecuencyBlue')}
+      <div className='item grid'>
+        <input 
+          type='text' 
+          id='frecuencyBlue' 
+          value={frecuencyBlue} 
+          onBlur={() => actualizarVariables()} 
+          onChange={(e) => setFrecuencyBlue(e.target.value)} 
+          onKeyDown={(event) => checkEnter(event, 'frecuencyBlue')} 
         />
       </div>
-      <div className="item grid">
-        <span id="angleItem" style={{ cursor: 'help' }}>Angle</span>
+      <div className='item grid'>
+        <span id='angleItem' style={{ cursor: 'help' }}>Angle</span>
       </div>
-      <div className="item grid">
-        <input
-          type="text"
-          value={angleRed}
-          onChange={(e) => setAngleRed(e.target.value)}
-          onBlur={actualizarVariables}
-          onKeyDown={(e) => checkEnter(e, 'angleRed')}
+      <div className='item grid'>
+        <input 
+          type='text' 
+          id='angleRed' 
+          value={angleRed} 
+          onBlur={() => actualizarVariables()} 
+          onChange={(e) => setAngleRed(e.target.value)} 
+          onKeyDown={(event) => checkEnter(event, 'angleRed')} 
         />
       </div>
-      <div className="item grid">
-        <input
-          type="text"
-          value={angleGreen}
-          onChange={(e) => setAngleGreen(e.target.value)}
-          onBlur={actualizarVariables}
-          onKeyDown={(e) => checkEnter(e, 'angleGreen')}
+      <div className='item grid'>
+        <input 
+          type='text' 
+          id='angleGreen' 
+          value={angleGreen} 
+          onBlur={() => actualizarVariables()} 
+          onChange={(e) => setAngleGreen(e.target.value)} 
+          onKeyDown={(event) => checkEnter(event, 'angleGreen')} 
         />
       </div>
-      <div className="item grid">
-        <input
-          type="text"
-          value={angleBlue}
-          onChange={(e) => setAngleBlue(e.target.value)}
-          onBlur={actualizarVariables}
-          onKeyDown={(e) => checkEnter(e, 'angleBlue')}
+      <div className='item grid'>
+        <input 
+          type='text' 
+          id='angleBlue' 
+          value={angleBlue} 
+          onBlur={() => actualizarVariables()} 
+          onChange={(e) => setAngleBlue(e.target.value)} 
+          onKeyDown={(event) => checkEnter(event, 'angleBlue')} 
         />
       </div>
-      <div className="item grid">
-        <span id="intensityItem" style={{ cursor: 'help' }}>Intensity</span>
+      <div className='item grid'>
+        <span id='intensityItem' style={{ cursor: 'help' }}>Intensity</span>
       </div>
-      <div className="item grid">
-        <input
-          type="text"
-          value={intensityRed}
-          onChange={(e) => setIntensityRed(e.target.value)}
-          onBlur={actualizarVariables}
-          onKeyDown={(e) => checkEnter(e, 'intensityRed')}
+      <div className='item grid'>
+        <input 
+          type='text' 
+          id='intensityRed' 
+          value={intensityRed} 
+          onBlur={() => actualizarVariables()} 
+          onChange={(e) => setIntensityRed(e.target.value)} 
+          onKeyDown={(event) => checkEnter(event, 'intensityRed')} 
         />
       </div>
-      <div className="item grid">
-        <input
-          type="text"
-          value={intensityGreen}
-          onChange={(e) => setIntensityGreen(e.target.value)}
-          onBlur={actualizarVariables}
-          onKeyDown={(e) => checkEnter(e, 'intensityGreen')}
+      <div className='item grid'>
+        <input 
+          type='text' 
+          id='intensityGreen' 
+          value={intensityGreen} 
+          onBlur={() => actualizarVariables()} 
+          onChange={(e) => setIntensityGreen(e.target.value)} 
+          onKeyDown={(event) => checkEnter(event, 'intensityGreen')} 
         />
       </div>
-      <div className="item grid">
-        <input
-          type="text"
-          value={intensityBlue}
-          onChange={(e) => setIntensityBlue(e.target.value)}
-          onBlur={actualizarVariables}
-          onKeyDown={(e) => checkEnter(e, 'intensityBlue')}
+      <div className='item grid'>
+        <input 
+          type='text' 
+          id='intensityBlue' 
+          value={intensityBlue} 
+          onBlur={() => actualizarVariables()} 
+          onChange={(e) => setIntensityBlue(e.target.value)} 
+          onKeyDown={(event) => checkEnter(event, 'intensityBlue')} 
         />
       </div>
     </div>
