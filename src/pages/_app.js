1fe38a54d8f3c7a3cc5548ba8a62cc93f6ecad72
Jorge <hello@jhonboy.com>
2024-09-14 20:40:18 +0100

    🐛 BUG FIX - Viewport meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,14 @@
 // src/pages/_app.js
 import "../styles/styles.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="author" content="Jhon Boy" />
         <meta name="robots" content="index,follow" />
