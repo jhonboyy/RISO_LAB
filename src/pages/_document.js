c9d5438b0dd86ade25696a0b8a9a189de4a4c821
Jorge <hello@jhonboy.com>
2024-09-14 13:10:08 +0100

    🔨 REFACTOR - Scripts integration

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,3 @@
-// src/pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -10,22 +9,38 @@ export default function Document() {
         <meta name="theme-color" content="#ffffff" />
         <meta name="author" content="Jhon Boy" />
         <meta name="robots" content="index,follow" />
-        <meta name="description" content="Jhon Boy Riso Lab is an experimental app that improves the process of risograph printing" />
+        <meta
+          name="description"
+          content="Jhon Boy Riso Lab is an experimental app that improves the process of risograph printing"
+        />
         <meta property="og:url" content="https://jhonboyy.github.io/riso_lab/" />
         <meta property="og:title" content="Jhon Boy" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Jhon Boy Riso Lab" />
-        <meta name="keywords" content="jhon boy, riso, risograph, printing, app, tool" />
-        <link rel="canonical" href="https://jhonboyy.github.io/riso_lab/" />
+        <meta
+          name="keywords"
+          content="jhon boy, riso, risograph, printing, app, tool"
+        />
+        <link
+          rel="canonical"
+          href="https://jhonboyy.github.io/riso_lab/"
+        />
         <link rel="icon" href="/images/jhon-boy-favicon.svg" />
-        <link rel="mask-icon" href="/images/jhon-boy-favicon.svg" color="#FFFFFF" />
+        <link
+          rel="mask-icon"
+          href="/images/jhon-boy-favicon.svg"
+          color="#FFFFFF"
+        />
         <link rel="apple-touch-icon" href="/images/jhon-boy-favicon.svg" />
-        <title>Jhon Boy Riso Lab</title>
+        <script src="/p5.min.js"></script>
+        <script src="/p5.riso.js"></script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
+      <script src="/scripts.js"></script>
+      <script src="/final.js"></script>
     </Html>
   );
 }
