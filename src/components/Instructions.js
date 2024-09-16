3687b9b4d00b65b477b20b5532c787ab067faa1a
Jorge <hello@jhonboy.com>
2024-09-16 11:13:08 +0100

    🐛 BUG FIX - Deploy

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,15 +1,15 @@
 export default function Instructions() {
-    return (   
-      <p id="menuText">
-        Welcome to John Boy Riso Lab! With this app, you can submit your own pictures, transform the RGB channels into risograph colors and download them for printing:
-        <br />
-        <br /> 1. Upload your photo.
-        <br /> 2. Choose from over 70 colors for the main channels.
-        <br /> 3. Select a pattern for the risograph texture.
-        <br /> 4. Play around with and modify the values of the textures.
-        <br /> 5. Once you're satisfied, click "Download result" to download your model, conveniently divided into layers.
-        <br />
-        <br /> Credits: P5.RISO, Dinamo Typefaces.
-      </p>
-    );  
-  }
+  return (   
+    <p id="menuText">
+      Welcome to John Boy Riso Lab! With this app, you can submit your own pictures, transform the RGB channels into risograph colors and download them for printing:
+      <br />
+      <br /> 1. Upload your photo.
+      <br /> 2. Choose from over 70 colors for the main channels.
+      <br /> 3. Select a pattern for the risograph texture.
+      <br /> 4. Play around with and modify the values of the textures.
+      <br /> 5. Once you&apos;re satisfied, click &quot;Download result&quot; to download your model, conveniently divided into layers.
+      <br />
+      <br /> Credits: P5.RISO, Dinamo Typefaces.
+    </p>
+  );  
+}
